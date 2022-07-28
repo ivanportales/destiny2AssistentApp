f1dbc0126c09e62de1d6efdd4210dd75ac00a91f
ivanportales <gidsp.snf19@uea.edu.br>
2022-07-27 23:34:03 -0300

    adding the URLItem and APIConstants enums to help build the URLRequests

--- destiny2AssistentApp/Domain/Services/RequestFactory.swift
+++ destiny2AssistentApp/Domain/Services/RequestFactory.swift
@@ -12,16 +12,35 @@ protocol RequestFactoryProtocol {}
 struct RequestFactory: RequestFactoryProtocol {
     
     private let APIKey = "752b9e510a124a89ab4efa4caed70457"
+    private let constants: APIConstants
     
-    private func makeRequest(to url: URL,
-                             withHeaders headers: [String: String]) -> URLRequest {
+    init(constants: APIConstants) {
+        self.constants = constants
+    }
+    
+    func makeURLRequest(item: URLItem) -> URLRequest {
+        let url = item.buildURL()
+        
         var request = URLRequest(url: url)
         request.addValue("X-API-Key", forHTTPHeaderField: APIKey)
         
-        for (key, value) in headers {
+        for (key, value) in item.headers {
             request.addValue(key, forHTTPHeaderField: value)
         }
         
         return request
+        
     }
+}
+
+protocol APIConstants {
+    var host: String { get }
+    var scheme: String { get }
+    var apiKey: String { get }
+}
+
+protocol URLItem {
+    var headers: [String: String] { get }
+    
+    func buildURL() -> URL
 }
