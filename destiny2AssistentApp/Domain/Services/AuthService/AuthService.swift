a435dde70048efd1e58884e66314361de794992a
ivanportales <gidsp.snf19@uea.edu.br>
2022-07-31 22:18:01 -0300

    started to implement the login with OAuth2

---
+++ destiny2AssistentApp/Domain/Services/AuthService/AuthService.swift
@@ -0,0 +1,79 @@
+//
+//  AuthService.swift
+//  destiny2AssistentApp
+//
+//  Created by Gonzalo Ivan Santos Portales on 31/07/22.
+//
+
+import Foundation
+
+enum AuthRequest: RequestProtocol {
+    
+    case signIn
+    
+    var headers: [String : String] {
+        switch self {
+        case .signIn:
+            return [:]
+        }
+    }
+    
+    var scheme: HTTPScheme {
+        switch self {
+        case .signIn:
+            return .https
+        }
+    }
+    
+    var path: String {
+        switch self {
+        case .signIn:
+            return "www.bungie.net/en/oauth/authorize"
+        }
+    }
+    
+    var queriesParameters: [String : String] {
+        switch self {
+        case .signIn:
+            return [
+                "client_id": "40896",
+                "response_type": "code",
+                "state": "testando"
+            ]
+        }
+    }
+    
+    var httpMethod: HTTPMethod {
+        switch self {
+        case .signIn:
+            return .get
+        }
+    }
+}
+
+protocol ServiceProtocol {
+    func send<ResultType: Decodable>(request: URLRequest, completion: @escaping (Result<ResultType, Error>) -> Void)
+}
+
+class AuthService {
+    
+    private let requestFactory: RequestFactory
+    private let service: ServiceProtocol
+    
+    init(service: ServiceProtocol,
+         requestFactory: RequestFactory) {
+        self.service = service
+        self.requestFactory = requestFactory
+    }
+    
+    func login(completion: @escaping (Result<String, Error>) -> Void) throws {
+        do {
+            let requestType: AuthRequest = .signIn
+            let request = try requestFactory.make(request: requestType)
+            
+            service.send(request: request, completion: completion)
+        } catch let error {
+            throw error
+        }
+    }
+}
---
+++ destiny2AssistentApp/Domain/Services/OAuthService.swift
@@ -0,0 +1,238 @@
+//
+//  OAuthService.swift
+//  destiny2AssistentApp
+//
+//  Created by Gonzalo Ivan Santos Portales on 31/07/22.
+//
+
+import Foundation
+
+struct AuthResponse: Codable {
+    let access_token: String
+    let expires_in: Int
+    let refresh_token: String?
+    let scope: String
+    let token_type: String
+}
+
+final class OAuth2Service {
+    static let shared = OAuth2Service()
+
+    private var refreshingToken = false
+
+    /* Varia de projeto para projeto */
+    struct Constants {
+        static let clientID = "40896" // changeble
+        static let clientSecret = "515117035e88cb15476e492ce02631ce7cface00" // changeble
+        static let tokenAPIURL = "https://github.com/login/oauth/access_token" // changeble
+        static let redirectURI = "authhub" // changeble
+        static let scopes = "" // changeble
+    }
+
+    public var signInURL: URL? {
+        //let base = "https://github.com/login/oauth/authorize?client_id=\(Constants.clientID)"
+        let base = "https://www.bungie.net/en/oauth/authorize?client_id=\(Constants.clientID)&response_type=code"
+       
+        return URL(string: base)
+    }
+    
+    /* Aqui */
+    
+    private init() {}
+
+    var isSignedIn: Bool {
+        return accessToken != nil
+    }
+
+    private var accessToken: String? {
+        return UserDefaults.standard.string(forKey: "access_token")
+    }
+
+    private var refreshToken: String? {
+        return UserDefaults.standard.string(forKey: "refresh_token")
+    }
+
+    private var tokenExpirationDate: Date? {
+        return UserDefaults.standard.object(forKey: "expirationDate") as? Date
+    }
+
+    private var shouldRefreshToken: Bool {
+        guard let expirationDate = tokenExpirationDate else {
+            return false
+        }
+        let currentDate = Date()
+        let fiveMinutes: TimeInterval = 300
+        return currentDate.addingTimeInterval(fiveMinutes) >= expirationDate
+    }
+
+    public func exchangeCodeForToken(
+        code: String,
+        completion: @escaping ((Bool) -> Void)
+    ) {
+        // Get Token
+        guard let url = URL(string: Constants.tokenAPIURL) else {
+            return
+        }
+
+        var components = URLComponents()
+        components.queryItems = [
+            URLQueryItem(name: "grant_type",
+                         value: "authorization_code"),
+            URLQueryItem(name: "code",
+                         value: code),
+            URLQueryItem(name: "redirect_uri",
+                         value: Constants.redirectURI),
+        ]
+
+        var request = URLRequest(url: url)
+        request.httpMethod = "POST"
+        request.setValue("application/x-www-form-urlencoded ",
+                         forHTTPHeaderField: "Content-Type")
+        request.httpBody = components.query?.data(using: .utf8)
+
+        let basicToken = Constants.clientID+":"+Constants.clientSecret
+        let data = basicToken.data(using: .utf8)
+        guard let base64String = data?.base64EncodedString() else {
+            print("Failure to get base64")
+            completion(false)
+            return
+        }
+
+        request.setValue("Basic \(base64String)",
+                         forHTTPHeaderField: "Authorization")
+
+        let task = URLSession.shared.dataTask(with: request) { [weak self] data, response, error in
+            guard let data = data,
+                  error == nil else {
+                completion(false)
+                return
+            }
+            let httpResponse = response as! HTTPURLResponse
+            
+            do {
+                let result = try JSONDecoder().decode(AuthResponse.self, from: data)
+                self?.cacheToken(result: result)
+                completion(true)
+            }
+            catch {
+                print(error.localizedDescription)
+                completion(false)
+            }
+        }
+        task.resume()
+    }
+
+    private var onRefreshBlocks = [((String) -> Void)]()
+
+    /// Supplies valid token to be used with API Calls
+    public func withValidToken(completion: @escaping (String) -> Void) {
+        guard !refreshingToken else {
+            // Append the compleiton
+            onRefreshBlocks.append(completion)
+            return
+        }
+
+        if shouldRefreshToken {
+            // Refresh
+            refreshIfNeeded { [weak self] success in
+                if let token = self?.accessToken, success {
+                    completion(token)
+                }
+            }
+        }
+        else if let token = accessToken {
+            completion(token)
+        }
+    }
+
+    public func refreshIfNeeded(completion: ((Bool) -> Void)?) {
+        guard !refreshingToken else {
+            return
+        }
+
+        guard shouldRefreshToken else {
+            completion?(true)
+            return
+        }
+
+        guard let refreshToken = self.refreshToken else{
+            return
+        }
+
+        // Refresh the token
+        guard let url = URL(string: Constants.tokenAPIURL) else {
+            return
+        }
+
+        refreshingToken = true
+
+        var components = URLComponents()
+        components.queryItems = [
+            URLQueryItem(name: "grant_type",
+                         value: "refresh_token"),
+            URLQueryItem(name: "refresh_token",
+                         value: refreshToken),
+        ]
+
+        var request = URLRequest(url: url)
+        request.httpMethod = "POST"
+        request.setValue("application/x-www-form-urlencoded ",
+                         forHTTPHeaderField: "Content-Type")
+        request.httpBody = components.query?.data(using: .utf8)
+
+        let basicToken = Constants.clientID+":"+Constants.clientSecret
+        let data = basicToken.data(using: .utf8)
+        guard let base64String = data?.base64EncodedString() else {
+            print("Failure to get base64")
+            completion?(false)
+            return
+        }
+
+        request.setValue("Basic \(base64String)",
+                         forHTTPHeaderField: "Authorization")
+
+        let task = URLSession.shared.dataTask(with: request) { [weak self] data, _, error in
+            self?.refreshingToken = false
+            guard let data = data,
+                  error == nil else {
+                completion?(false)
+                return
+            }
+
+            do {
+                let result = try JSONDecoder().decode(AuthResponse.self, from: data)
+                self?.onRefreshBlocks.forEach { $0(result.access_token) }
+                self?.onRefreshBlocks.removeAll()
+                self?.cacheToken(result: result)
+                completion?(true)
+            }
+            catch {
+                print(error.localizedDescription)
+                completion?(false)
+            }
+        }
+        task.resume()
+    }
+
+    private func cacheToken(result: AuthResponse) {
+        UserDefaults.standard.setValue(result.access_token,
+                                       forKey: "access_token")
+        if let refresh_token = result.refresh_token {
+            UserDefaults.standard.setValue(refresh_token,
+                                           forKey: "refresh_token")
+        }
+        UserDefaults.standard.setValue(Date().addingTimeInterval(TimeInterval(result.expires_in)),
+                                       forKey: "expirationDate")
+    }
+
+    public func signOut(completion: (Bool) -> Void) {
+        UserDefaults.standard.setValue(nil,
+                                       forKey: "access_token")
+        UserDefaults.standard.setValue(nil,
+                                       forKey: "refresh_token")
+        UserDefaults.standard.setValue(nil,
+                                       forKey: "expirationDate")
+
+        completion(true)
+    }
+}
